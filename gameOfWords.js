30cb4aa2a2d27cfa5da5d1557d0c92f592d488aa
BartholomewKuma27 <igugu13@freeuni.edu.ge>
2016-09-07 11:13:08 +0400

    small change to design

--- gameOfWords.js
+++ gameOfWords.js
@@ -1,4 +1,11 @@
 document.getElementById("offlineLink").onclick = function (){
 	document.getElementById("mainPageDiv").style.display = "none";
+	sessionStorage.setItem("mainPageDisplay", "none");
 }
 
+window.onload = function(){
+	var mainPageDisplay = sessionStorage.getItem('mainPageDisplay');
+	document.getElementById("mainPageDiv").style.display = mainPageDisplay;
+}
+
+//mtavaria ar damaviwydes tamashis bolos shemdegi kodi: sessionStorage.setItem("mainPageDisplay", "flex");
